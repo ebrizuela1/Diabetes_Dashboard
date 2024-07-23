c46a529073fdca053ae5077b82a15f898dca3362
ebrizuela1 <106275585+ebrizuela1@users.noreply.github.com>
2024-07-23 14:48:34 -0500

    custom image per question

--- DR_Self_Screen.py
+++ DR_Self_Screen.py
@@ -82,7 +82,7 @@ def main():
                         st.experimental_rerun()
 
             with col2:
-                st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80")  # Placeholder image
+                st.image(question["image"], use_column_width=True)
 
             # Dynamic progress bar at the bottom
             progress = (st.session_state.current_question + 1) / len(questions)
